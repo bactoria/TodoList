2d523ae7e4977a3b3b942f4835c32c72aece3b83
bactoria <bactoria@gmail.com>
2018-11-02 19:46:39 +0900

    Backend :: jpa auditing 설정 분리
    기존에 Application.java 파일의 @EnableJpaAuditing 때문에
    controller 단위테스트 시에 에러가 뜸
    그래서, 따로 config 파일 만들었음

---
+++ Backend/src/main/java/me/bactoria/todoList/config/JpaAuditConfig.java
@@ -0,0 +1,10 @@
+package me.bactoria.todoList.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableJpaAuditing
+public class JpaAuditConfig {
+
+}

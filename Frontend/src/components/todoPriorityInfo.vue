91ad57aa1f17df16db40843c597cd54d1090ad34
bactoria <bactoria@gmail.com>
2018-11-04 20:17:06 +0900

    Frontend :: 오탈자 수정

--- Frontend/src/components/todoPriorityInfo.vue
+++ Frontend/src/components/todoPriorityInfo.vue
@@ -2,7 +2,7 @@
     <div class="GodoB" style="position: fixed; bottom: 30px; left: 30px;">
         <div>
             <h2>Priority</h2>
-            <div><priority priority=1 /> 1 순위 : </div>
+            <div><priority priority=1 /> : 1 순위</div>
             <div><priority priority=2 /> : 2 순위</div>
             <div><priority priority=3 /> : 3 순위</div>
         </div>

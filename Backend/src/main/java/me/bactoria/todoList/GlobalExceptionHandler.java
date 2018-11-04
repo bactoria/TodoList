79092159a3b50867b145c1f64ce90af89c84b8a9
bactoria <bactoria@gmail.com>
2018-11-04 17:27:25 +0900

    Backend :: validation 핸들러 추가

--- Backend/src/main/java/me/bactoria/todoList/GlobalExceptionHandler.java
+++ Backend/src/main/java/me/bactoria/todoList/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package me.bactoria.todoList;
 
+import lombok.Getter;
 import me.bactoria.todoList.todo.exception.TodoNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,4 +17,33 @@ public class GlobalExceptionHandler {
     String todoNotFoundException(TodoNotFoundException e) {
         return "{\"exception\" : \"TodoNotFoundException\", \"message\" : \"해당 Todo가 존재하지 않습니다.\"}";
     }
+
+    @ExceptionHandler
+    public ResponseEntity<String> asd(MethodArgumentNotValidException e) {
+
+        ErrorDetails errorDetails = new ErrorDetails(e.getBindingResult().getFieldError().getField(), e.getBindingResult().getFieldError().getDefaultMessage());
+
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @Getter
+    private class ErrorDetails {
+
+        private String field;
+        private String message;
+
+        public ErrorDetails(String field, String message) {
+            this.field = field;
+            this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            return "ErrorDetails{" +
+                    "errorField='" + field + '\'' +
+                    ", message='" + message + '\'' +
+                    '}';
+        }
+    }
+
 }
